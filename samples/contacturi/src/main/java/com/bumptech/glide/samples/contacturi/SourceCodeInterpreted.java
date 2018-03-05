ba2b0b5b0c865041f5f7696db318f61ee2e42a0d
jinfeng@uama.com.cn <feng5320@uama>
2018-03-06 01:25:33 +0800

    test

--- samples/contacturi/src/main/java/com/bumptech/glide/samples/contacturi/SourceCodeInterpreted.java
+++ samples/contacturi/src/main/java/com/bumptech/glide/samples/contacturi/SourceCodeInterpreted.java
@@ -12,7 +12,6 @@ import com.bumptech.glide.Glide;
  */
 public class SourceCodeInterpreted extends Activity {
 
-
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
